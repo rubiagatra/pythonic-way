8d1ce0c38b70643106c74997ca44d930897d596f
Doni Rubiagatra <rubiagatra@gmail.com>
2017-07-25 20:04:50 +0700

    next commit

--- credit_card/credit_card.py
+++ credit_card/credit_card.py
@@ -32,5 +32,23 @@ class CreditCard:
     def make_payment(self, amount):
         self._balance -= amount
 
-if __name == "__main__":
+
+class PredatoryCreditCard(CreditCard):
+    def __init__(self, customer, bank, acnt, limit, apr):
+        super().__init__(customer, bank, acnt, limit)
+        self._apr = apr
+
+    def charge(self, price):
+        success = super().charge(price)
+        if not success:
+            self._balance += 5
+        return success
+
+    def process_montH(self):
+        if self._balance > 0:
+            monthly_factor = pow(1 + self._apr, 1/12)
+            self._balance *= monthly_factor
+
+
+if __name__ == "__main__":
     pass
