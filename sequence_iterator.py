cd4d53a5991ff3918118ca32006358c2a2c05017
Doni Rubiagatra <rubiagatra@gmail.com>
2017-07-25 18:36:33 +0700

    next commit

---
+++ sequence_iterator.py
@@ -0,0 +1,21 @@
+class SequenceIterator:
+
+    def __init__(self, sequence):
+        self._seq = sequence
+        self._k = -1
+
+    def __next__(self):
+        self._k += 1
+        if self._k < len(self._seq):
+            return(self._seq[self._k])
+        else:
+            raise StopIteration()
+
+    def __iter__(self):
+        return self
+
+if __name__ == "__main__":
+    seq = SequenceIterator([1,2,3])
+    for i in iter(seq):
+        print(i)
+        print(next(seq))
